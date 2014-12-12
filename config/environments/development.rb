8da478012f2fac32c6416b22cfb2f3e25829a931
Henry J. Wylde <public@hjwylde.com>
2014-12-12 13:28:09 +1300

    Changed page cache directory
    Removed cached pages from being commited to git repo.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
-  config.action_controller.page_cache_directory = "#{Rails.root.to_s}/public/deploy"
+  config.action_controller.page_cache_directory = "#{Rails.root.to_s}/public/cache"
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
